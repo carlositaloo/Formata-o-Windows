9ff3fc83176394f43dde7c181efd86ebdfdd3f9e
carlositaloo <carlos_italo@email.com>
2023-07-10 11:47:27 -0300

    OK

--- WindowsDebloater/UninstallApps.ps1
+++ WindowsDebloater/UninstallApps.ps1
@@ -3,10 +3,10 @@ Set-ItemProperty -Path "HKCU:\Software\Microsoft\GameBar" -Name "ShowStartupPane
 
 # Lista de aplicativos que serão mantidos e não desinstalados (whitelist):
 [regex]$WhitelistedApps = 'Microsoft.WindowsStore|Microsoft.Windows.Photos|Microsoft.WindowsCalculator|Microsoft.ScreenSketch|Microsoft.WindowsSoundRecorder|Microsoft.DesktopAppInstaller|Microsoft.WindowsCamera|NVIDIACorp.NVIDIAControlPanel|Microsoft.Paint|Microsoft.MicrosoftEdge.Stable|Microsoft.WindowsNotepad|Microsoft.XboxIdentityProvider|Microsoft.ZuneMusic|MicrosoftCorporationII.QuickAssist|WinRAR.ShellExtension|Microsoft.WindowsTerminal'
-        
+
 # Obtém a lista de pacotes de aplicativos instalados em todos os usuários e remove os pacotes que não correspondem aos aplicativos listados em $WhitelistedApps:
 Get-AppxPackage -AllUsers | Where-Object {$_.Name -NotMatch $WhitelistedApps} | Remove-AppxPackage | out-Null
-        
+
 # Obtém a lista de pacotes Online instalados em todos os usuários e remove os pacotes que não correspondem aos aplicativos listados em $WhitelistedApps:
 Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -NotMatch $WhitelistedApps} | Remove-AppxProvisionedPackage -Online | out-Null
 
