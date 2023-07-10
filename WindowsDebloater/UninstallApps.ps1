e6fbe52a7aac28bae1682a21bc92deb06e381bae
carlositaloo <carlos_italo@email.com>
2023-07-10 11:46:52 -0300

    OK

--- WindowsDebloater/UninstallApps.ps1
+++ WindowsDebloater/UninstallApps.ps1
@@ -1,3 +1,5 @@
+# Desativa overlay pedindo para baixar o Xbox Game Bar quando abrir um jogo, pois ele serar desinstalado.
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\GameBar" -Name "ShowStartupPanel" -Value 0
 
 # Lista de aplicativos que serão mantidos e não desinstalados (whitelist):
 [regex]$WhitelistedApps = 'Microsoft.WindowsStore|Microsoft.Windows.Photos|Microsoft.WindowsCalculator|Microsoft.ScreenSketch|Microsoft.WindowsSoundRecorder|Microsoft.DesktopAppInstaller|Microsoft.WindowsCamera|NVIDIACorp.NVIDIAControlPanel|Microsoft.Paint|Microsoft.MicrosoftEdge.Stable|Microsoft.WindowsNotepad|Microsoft.XboxIdentityProvider|Microsoft.ZuneMusic|MicrosoftCorporationII.QuickAssist|WinRAR.ShellExtension|Microsoft.WindowsTerminal'
