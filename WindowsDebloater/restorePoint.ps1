8c4b8041b04c1ca0f495e5781908be6c0a730e29
iNooTh <carlos_italo@email.com>
2023-08-14 13:22:07 -0300

    OK

--- WindowsDebloater/restorePoint.ps1
+++ WindowsDebloater/restorePoint.ps1
@@ -6,16 +6,18 @@ if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 
 # Cria um ponto de restauração
 $description = "Windows Debloater - Ponto de Restauração"
-$restorePoint = Get-WmiObject -Class Win32_RestorePoint -Namespace "root/default" |
-                Where-Object { $_.Description -eq $description }
 
-if (-not $restorePoint) {
-    Invoke-CimMethod -Namespace "root/default" -ClassName "SystemRestore" -MethodName "CreateRestorePoint" -Arguments @{
-        Description = $description
-        RestorePointType = 12
-        EventType = 100
-    }
-    Write-Host "Ponto de restauração criado com sucesso."
-} else {
-    Write-Host "Já existe um ponto de restauração com essa descrição."
+$arguments = @{
+    Description = $description
+    RestorePointType = 12
+    EventType = 100
 }
+
+$systemRestoreClass = Get-CimClass -Namespace "root/default" -ClassName "SystemRestore"
+$methodParameters = $systemRestoreClass.CimClassMethods['CreateRestorePoint'].Parameters
+
+$methodParameters['EventType'].Value = 100
+
+Invoke-CimMethod -Namespace "root/default" -ClassName "SystemRestore" -MethodName "CreateRestorePoint" -Arguments $methodParameters
+
+Write-Host "Ponto de restauração criado com sucesso."
